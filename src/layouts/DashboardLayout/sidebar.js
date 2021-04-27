e4e9a4dcac70c9d9070110c71d4eba1432ef1e75
Patradanai <patradanai@Patradanais-MacBook-Pro.local>
2021-04-27 13:26:21 +0700

    add icon list sidebar

--- src/layouts/DashboardLayout/sidebar.js
+++ src/layouts/DashboardLayout/sidebar.js
@@ -1,19 +1,46 @@
 import React from "react";
+import Link from "next/link";
 import ShopIcon from "../../components/icons/Shop";
+import HomeIcon from "../../components/icons/Home";
+import SettingIcon from "../../components/icons/Setting";
+import TeamIcon from "../../components/icons/Team";
+import ProductIcon from "../../components/icons/Product";
+import OrderIcon from "../../components/icons/Order";
+import HistoryIcon from "../../components/icons/History";
+
 const SideBar = () => {
   return (
-    <div className="w-1/4 py-5 px-3" style={{ background: "#43379c" }}>
+    <div className="w-1/4 py-5 px-5" style={{ background: "#43379c" }}>
       <div className="text-xl flex items-center">
-        <ShopIcon className="w-10 h-10 bg-gray-400" />
+        <ShopIcon className="w-10 h-10 text-gray-400" />
         <p className="text-white font-sans ml-2 font-semibold">E-Commerce</p>
       </div>
       <div className="text-white mt-5">
         <ul>
-          <li>Dashboard</li>
-          <li>Staff Management</li>
-          <li>Product</li>
-          <li></li>
-          <li></li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <HomeIcon className="w-6 h-6 text-gray-400 mr-2" />
+            Dashboard
+          </li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <TeamIcon className="w-6 h-6 text-gray-400 mr-2" />
+            Staff Management
+          </li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <ProductIcon className="w-6 h-6 text-gray-400 mr-2" />
+            Product
+          </li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <OrderIcon className="w-6 h-6 text-gray-400 mr-2" />
+            Order
+          </li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <HistoryIcon className="w-6 h-6 text-gray-400 mr-2" />
+            History
+          </li>
+          <li className="h-10 flex items-center hover:bg-gray-200 hover:bg-opacity-40 rounded px-2">
+            <SettingIcon className="w-6 h-6 text-gray-400 mr-2" />
+            Setting
+          </li>
         </ul>
       </div>
     </div>
