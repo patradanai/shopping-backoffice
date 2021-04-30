6eb680ed9679e19cc2b14b4772fa6d834a1005ef
Patradanai <patradanai@Patradanais-MacBook-Pro.local>
2021-04-30 23:47:04 +0700

    add card

--- src/pages/Dashboard/History/ListLog/index.js
+++ src/pages/Dashboard/History/ListLog/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import moment from "moment";
 const ListLog = (props) => {
   return (
     <tr className="text-center bg-white">
       <td className="py-2 px-2">{props.logs.id}</td>
-      <td className="py-2 px-2">{props.logs.createdAt}</td>
+      <td className="py-2 px-2">
+        {moment(props.logs.createdAt).format("MMM-DD-YYYY hh:mm")}
+      </td>
       <td className="py-2 px-2">{props.logs.User?.email}</td>
       <td className="py-2 px-2">{props.logs.eventType}</td>
       <td className="py-2 px-2">{props.logs.type}</td>
